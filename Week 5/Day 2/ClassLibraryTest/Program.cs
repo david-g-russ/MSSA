efb6e048870d41f88ace5349c8b6ce32ac2f8481
david-g-russ <david.georg.russ@gmail.com>
2022-02-08 08:37:31 -0800

    New test files added.

---
+++ Week 5/Day 2/ClassLibraryTest/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> intList = new List<int>() { 100,2,3,49,8123};
+            intList.Sort();
+            foreach (int i in intList)
+            {
+                Console.WriteLine(i);
+            }
+
+
+            List<Student> studentList = new List<Student>();
+            studentList.Add(new Student() { Name = "Kelly", Grades = 80, Address = "123"});
+            studentList.Add(new Student() { Name = "Andrew", Grades = 60, Address = "123" });
+            studentList.Add(new Student() { Name = "Will", Grades = 85, Address = "123" });
+            studentList.Add(new Student() { Name = "Chris", Grades = 90, Address = "123" });
+
+            studentList.Sort();
+            foreach (var s in studentList)
+            {
+                Console.WriteLine($"{s.Name}, {s.Address}, {s.Grades}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
