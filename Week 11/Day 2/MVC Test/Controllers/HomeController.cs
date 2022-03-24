18ab272872ae528e6baa3bf197718974ce4fc4c1
David Russ <david.georg.russ@gmail.com>
2022-03-24 14:53:58 -0700

    Additional assignments

---
+++ Week 11/Day 2/MVC Test/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_Test.Controllers
+{
+    public class HomeController : Controller
+    {
+        // multiple methods
+
+        // http://localhost:3323/controller/action
+        public IActionResult Index()
+        {
+            return View(); // by default search for Index.cshtml
+        }
+        public IActionResult Test()
+        {
+            return View();
+        }
+    }
+}
